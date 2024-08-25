3d4e0aa5e8c2b0b0a549feca981b17562a84c140
Sahil Shamnani <sahilcodes1@gmail.com>
2024-08-25 22:40:28 +0000

    --

--- basics/func.js
+++ basics/func.js
@@ -1,9 +1,9 @@
 function calculateprice(...num1)
 {return num1}
 
-console.log(calculateprice(40, 80, 120, 160))
+console.log(calculateprice(40, 80, 120, 160, 150))
 
-const arr = calculateprice(40, 80, 120, 160);
+const arr = calculateprice(40, 80, 120, 160, 150);
 let sum = 0;
 for (let i = 0; i < arr.length; i++) {
   sum += arr[i];
---
+++ basics/loops.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  console.log(`i=`,i); 
+     for (let j = 0; j <= 10; j++) {
+      const element = j;
+      console.log(`${i}*${j}=`, i*j);}}
+
