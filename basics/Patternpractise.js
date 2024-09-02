b3435eb074c0108614592e266c8d00f9b1681fa3
Sahil Shamnani <sahilcodes1@gmail.com>
2024-09-02 12:28:45 +0000

    /

--- basics/Patternpractise.js
+++ basics/Patternpractise.js
@@ -1,9 +1,9 @@
 
 let n = 5;
-/*for (let i = 1; i <= n; i++) {
+for (let i = 1; i <= n; i++) {
     let str = "* ";
     console.log(str.repeat(i));
-}*/
+}
 
 for (let j = n; j >=0; j--) {
     let str="* ";
